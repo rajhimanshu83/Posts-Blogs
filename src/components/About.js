314417be5eed3a66b4c035ab9256ec6af897fc9f
Himanshu Raj <rajhimanshu83@gmail.com>
2019-03-01 01:41:17 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">About</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Rainbow(About);
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom'
+
+const Navbar = (props) => {
+  // setTimeout(() => {
+  //   props.history.push('/about');
+  // }, 2000);
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <a className="brand-logo">Poke' Times</a>
+        <ul className="right">
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
+        </ul>
+      </div>
+    </nav> 
+  )
+}
+export default withRouter(Navbar)
